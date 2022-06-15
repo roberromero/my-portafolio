412fcaf7e361c56b0610016d2dbe14c356f337f2
roberromero <robertoromerodeveloper@gmail.com>
2022-06-15 21:09:18 +0100

    header mobile done

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -6,19 +6,31 @@ import js from './img/js.png';
 import atom from './img/atom.png';
 import php from './img/php.png';
 import mysql from './img/mysql.png';
+import redux from './img/redux.svg';
+import nodejs from './img/nodejs.png';
 
 const Header = () => {
     const technologies = [html, css, js, atom, php, mysql];
-
+    
   return (
     <header>
+        <h1 id='name'>Roberto Romero</h1>
         <div className='container-header'>
-            {
-                technologies.map((img, index)=> {
-                     return <img className='tech-logos' src={img} key={index}/>
-                })
-            }
-            <p> 
+            <p className='header-paragraph'>Known technologies</p>
+            <div className='container__tech-logos'>
+                {
+                    technologies.map((img, index)=> {
+                        return <img className='tech-logos' src={img} key={index}/>
+                    })
+                }
+            </div>
+        <p className='line'></p>
+        <p className='header-paragraph'>Learning ...</p>
+        <div className='container__tech-logos'>
+            <img className='tech-logos' src={redux}/>
+            <img className='tech-logos' src={nodejs}/>
+        </div>
+            <p className='header-paragraph'> 
                 I am a qualified and professional web developer
                 with experience in website design. Strong creative
                 and analytical skills. I am currently building Apps to
