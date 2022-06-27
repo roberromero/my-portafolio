6d731ed458fe5da73de606f843b909e5c076d9fe
roberromero <robertoromerodeveloper@gmail.com>
2022-06-27 15:17:05 +0100

    addition to my profile

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -10,6 +10,13 @@ const Profile = () => {
         I am a qualified Full Stack Web Developer
         with experience in website design with strong creative
         and analytical skills.
+        I would define myself as a tireless worker, with great 
+        attention to detail, reliable, a great communicator and 
+        I love working in a team. I always show a high degree of 
+        commitment to the assigned tasks.
+        In addition to the technologies that I use on a daily basis 
+        and that appear in the header, I am also familiar with technologies 
+        such as XML, XHTML, JUnit, JQuery, Ajax, Java, C#, XAMPP, etc.
       </p>
       <button className='buttons'><a href={robertocv}>Download CV</a></button>
     </section>
