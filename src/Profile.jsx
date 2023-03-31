6d3ca119493770ae310df217135ebc26c7aaf2c0
roberromero <robertoromerodeveloper@gmail.com>
2023-03-31 10:45:44 +0100

    profile updated

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -7,16 +7,17 @@ const Profile = () => {
     <section className='profile'>
       <h2 className='title'>About me</h2>
       <p className='profile-paragraph'> 
-        I am a qualified Full Stack Web Developer
-        with experience in website design with strong creative
-        and analytical skills.
-        I would define myself as a tireless worker, with great 
-        attention to detail, reliable, a great communicator and 
+        I have just completed my course “Software Developer Level 4” by QA. I am looking for an
+        opportunity to boost my new career.
+        During this course we covered many different technologies such as Python, PHP, MySQL and Azure.
+        I am also familiarized with technologies such as XML, HTML, XHTML, CSS, Bootstrap, JavaScript,
+        JQuery, Ajax, MySQL, PL/SQL, XAMPP, etc. Since I completed a Diploma of Higher Education between
+        2020 and 2021.
+        Tireless worker, great attention to detail, reliable, and a great communicator are some of my attributes.
         I love working in a team. I always show a high degree of 
         commitment to the assigned tasks.
-        In addition to the technologies that I use on a daily basis 
-        and that appear in the header, I am also familiarised with technologies 
-        such as XML, XHTML, JUnit, JQuery, Ajax, Java, C#, XAMPP, etc.
+        After living in the UK for more than 10 years my level of English Language is excellent, and I am
+        willing to relocate if necessary.
       </p>
       <button className='buttons'><a href={robertocv} download>Download CV</a></button>
     </section>
